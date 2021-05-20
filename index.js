7c549cda46f54ebb45c3d5f68a7ff45216e06cb5
briangonzales10 <brian.gonzales10@gmail.com>
2021-05-20 11:45:16 -0500

    added unsplash random photos to post route

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let {destinations} = require('./db')
 const express = require('express')
-const {generateID} = require('./services')
+const {generateID, GrabImage} = require('./services')
 const PORT =  process.env.PORT || 3000;
 const app = express()
 
@@ -20,19 +20,23 @@ app.get('/destinations', (req, res) => {
 })
 
 // Post new objects via /destinations route
-app.post('/destinations', (req, res) =>{
+app.post('/destinations', async (req, res) =>{
+ 
+// const {name, location, photo, description} = req.body
+const {name, location, description} = req.body
+const photo = await GrabImage(name)
 
-const {name, location, photo, description} = req.query
 
 if (name === undefined || name.length === 0 || location === undefined || location.length === 0){
     return res.status(400).send("Error need destination name & location")
 }
-    console.log(name, location, photo, description);    
+
+    console.log(name, location, photo, description);     
     destinations.push({
         id: generateID(),
         name: name, 
         location: location,
-        photo: photo ? photo : "", 
+       photo: photo ? photo : "", 
         description: description ? description : ""
     })
     res.send("Post Request Sent")
@@ -52,20 +56,41 @@ app.delete('/destinations/:id', (req,res) => {
 })
 
 //Edit objects via destination route through 'query'
-app.put('/destinations', (req, res) =>{
+// app.put('/destinations', (req, res) =>{
 
-const {id, name, location, photo, description} = req.query
-// Filter for ID first
-for (let dest of destinations) {
-    if (dest.id === id) {
+// const {id, name, location, photo, description} = req.query
+// // Filter for ID first
+// for (let dest of destinations) {
+//     if (dest.id === id) {
         
+//         dest.name = name ? name : dest.name
+//         dest.location = location ? location : dest.location
+//         dest.photo = photo ? photo : dest.photo
+//         dest.description = description ? description : dest.description
+//         console.log(`${dest.name} | ${dest.location} | ${dest.photo} | ${dest.description} updated`);
+//     }
+   
+// } 
+//     res.send('PUT request updated')
+//   })
+
+app.put("/destinations/:id", (req, res) =>{
+    const {id} = req.params
+    const {name, location, photo, description} = req.body
+
+    if (!name && !location && !photo && !description){
+        return res.status.send('No data to update')
+    }
+
+    for (let dest of destinations) {
+        if (dest.id === id) {
         dest.name = name ? name : dest.name
         dest.location = location ? location : dest.location
         dest.photo = photo ? photo : dest.photo
         dest.description = description ? description : dest.description
         console.log(`${dest.name} | ${dest.location} | ${dest.photo} | ${dest.description} updated`);
+            break;
+        }
     }
-   
-} 
-    res.send('PUT request to homepage')
-  })
+    res.send('PUT request updated')
+})
--- services.js
+++ services.js
@@ -1,3 +1,4 @@
+const fetch = require('node-fetch')
 function generateID(){
 
     let id = ""
@@ -9,4 +10,18 @@ function generateID(){
    return id
 }
 
+async function GrabImage(destination) {
+    //Unsplash Images
+
+const API = `https://api.unsplash.com/search/photos/?client_id=9xB6KzPdxq603YtE7BwTp5OsWNjOrLPRsSw_4XWKW0A&query=`
+let dest = destination;
+const searchURL = `${API}${dest}`
+let ind = Math.floor(Math.random() * 10)
+return fetch(searchURL)
+    .then((response) => response.json())
+    .then((pics) =>  pics.results[ind].urls.small); // returns url
+    
+}
+
+exports.GrabImage = GrabImage
 exports.generateID = generateID
